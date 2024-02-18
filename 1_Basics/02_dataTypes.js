901074d105827c9f42167c6265fcf996151b1825
Anubhav-GEU <anubhav860102@gmail.com>
2024-02-18 22:23:30 +0530

    datatypes

---
+++ 1_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // js will use newer version of code below this 
+
+// alert(2+5);  it will throw error because alert is not available in node it will work in browser
+
+let name = "anubhav";
+let age = 18;
+let isLoggedIn = false;
+// primitive datatypes
+
+// range of number => 2^53 to increase the range use bigint
+
+// boolean => true/false
+// null => it is a standAlone value null is basically empty 
+// undefined => if variable is only declared but not initialised
+// symbol => to find uniqueness use this
+
+// not primitive
+// object
+
+console.log(typeof "Anubhav");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);
+
+//read ecma script
