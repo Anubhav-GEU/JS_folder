7b555a088e48e78a9569b8dffc3d1a3004ed1806
Anubhav-GEU <anubhav860102@gmail.com>
2024-02-21 23:42:20 +0530

    type conversions

--- 1_Basics/03_conversionOperations.js
+++ 1_Basics/03_conversionOperations.js
@@ -40,11 +40,28 @@ let str2 = " Anubhav";
 let str3 = str1+str2;
 console.log(str3);
 
+
+// conversion is mainly from left to right
 console.log("1"+2);
 console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");
+console.log(3+4*5%3);
 
 
 console.log(true);
+console.log(+true);
+// console.log(true+); this statement doesn't work
 console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+// num1 = num2 = num3 = 4
+console.log(num1,num2,num3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter++);
+console.log(gameCounter);
+
+// link to ecma script => https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
