e244435bcb3868cea0343ca9248d0bcaa4098ea4
Anubhav-GEU <anubhav860102@gmail.com>
2024-03-01 01:39:19 +0530

    oop

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,12 @@
+const user = {
+    username: 'anubhav',
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function() {
+        console.log(`username: ${this.username}`);
+        console.log(this);
+    }
+}
+console.log(user.username);
+console.log(user.getUserDetails);
